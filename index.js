8ee2312a39d73cc43fc3fa9aa9888d6a23c082fe
Omar <o.moreta@gmail.com>
2023-10-12 22:15:46 -0400

    added new teal route with teal html navbar

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ app.get("/", function (req, res) {
     `);
 });
 
+// Teal Page
+app.get("/teal", function (req, res) {
+  res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: teal;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: teal;">Teal Page</h1>
+        </body>
+    `);
+});
+
 // server is listening for requests on PORT
 app.listen(PORT, function () {
   console.log(`Express Server is alive on: ${PORT}`);
